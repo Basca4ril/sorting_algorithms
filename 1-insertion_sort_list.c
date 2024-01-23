273acf7e0a3ebd1887997647bda419785438e3db
Basca4ril <basianchls@gmail.com>
2024-01-23 18:57:31 +0200

    added task 1 file

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -26,8 +26,8 @@ void insertion_sort_list(listint_t **list)
 			tmp->prev = tmp->prev->prev;
 			tmp->next->prev = tmp;
 
-			if (tmp->prev == NULL)
-				*list = tmp;
+			/*if (tmp->prev == NULL)
+				*list = tmp;*/
 
 			print_list(*list);
 		}
