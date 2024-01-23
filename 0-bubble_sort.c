b7a4bb02e59558a7c0598d133abdcf985d180390
Basca4ril <basianchls@gmail.com>
2024-01-23 20:25:22 +0200

    added task 0 files

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,47 +1,36 @@
 #include "sort.h"
-
 /**
- * bubble_sort - Sorts an array of integers in ascending order
- * using the Bubble sort algorithm
+ * bubble_sort - sorts an array of integers using bubble sort
  *
- * @array: The array to be sorted
- * @size: Number of elements in the array
+ * @array: array to be sorted
+ * @size: size of the array
  */
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, j, k;
-    int temp;
-    int swapped;
-
-    if (array == NULL || size < 2)
-        return;
-
-    for (i = 0; i < size - 1; i++)
-    {
-        swapped = 0;
-
-        for (j = 0; j < size - i - 1; j++)
-        {
-            if (array[j] > array[j + 1])
-            {
-                /* Swap elements */
-                temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
-
-                /* Set flag to indicate a swap occurred */
-                swapped = 1;
+	size_t i, j, k;
+	int tmp, swp;
+	
+	if (array == NULL || size < 2)
+		return;
+	for (i = 0; i < size - 1; i++)
+	{
+		swp = 0;
 		
-                /* Print the array after swapping two elements */
-                for (k = 0; k < size - 1; k++)
-                    printf("%d, ", array[k]);
-                printf("%d\n", array[size - 1]);
-            }
-        }
-
-        /* If no swaps occurred, the array is already sorted */
-        if (swapped == 0)
-            break;
-    }
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+				swp = 1;
+				for (k = 0; k < size - 1; k++)
+					printf("%d, ", array[k]);
+				printf("%d\n", array[size - 1]);
+			}
+		}
+		if (swp == 0)
+			break;
+	}
 }
 
